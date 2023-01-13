a18d4b3bf56ce6bb9b1c29d1a0d5824e24588c80
Manu <manu.ravi@innovaturelabs.com>
2023-01-13 18:27:59 +0530

    User Table Created in Entity Class

--- Server/src/main/java/com/airline/reservation/entity/User.java
+++ Server/src/main/java/com/airline/reservation/entity/User.java
@@ -30,6 +30,84 @@ public class User {
     private   String city;
     @Column(nullable = true, length =50)
     private   String country;
+    public Integer getUserId() {
+        return userId;
+    }
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Date getDob() {
+        return dob;
+    }
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+    public String getPassport_number() {
+        return passport_number;
+    }
+    public void setPassport_number(String passport_number) {
+        this.passport_number = passport_number;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public Integer getStatus() {
+        return status;
+    }
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+    public Integer getRole() {
+        return role;
+    }
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+    public Date getCreateDate() {
+        return createDate;
+    }
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
     @Column(nullable = false,columnDefinition = "TINYINT")
     private   Integer status;
     @Column(nullable = false,columnDefinition = "TINYINT")
--- Server/src/main/java/com/airline/reservation/service/UserService.java
+++ Server/src/main/java/com/airline/reservation/service/UserService.java
@@ -1,5 +1,8 @@
 package com.airline.reservation.service;
 
+import com.airline.reservation.form.UserForm;
+import com.airline.reservation.view.UserView;
+
 public interface UserService {
-    
+    UserView add(UserForm form);
 }
