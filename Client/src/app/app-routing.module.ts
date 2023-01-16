b7fdf3644d7b7e27ffd3b8cdae4593fde6ced7e9
Aron <aron.jose@innovaturelabs.com>
2023-01-16 17:39:02 +0530

    first commit

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { CompanyRegistrationComponent } from './admin-home/company-registration/company-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:AdminHomeComponent},
+  {path:'companyreg',component:CompanyRegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { CompanyRegistrationComponent } from './admin-home/company-registration/company-registration.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminHomeComponent,
+    CompanyRegistrationComponent
+ 
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
