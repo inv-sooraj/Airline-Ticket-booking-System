4d66a19ffa5e49c4d54bb7b81c754af59bc30bbb
inv-lakshmi-mohan <lakshmi.mohan@innovaturelabs.com>
2023-02-22 10:20:21 +0530

    Change password(pasenger)

--- Client/src/app/user/header/header.component.ts
+++ Client/src/app/user/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControlOptions, Form, FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControlOptions, Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AlertService } from 'src/app/alert.service';
 import { ApiService } from 'src/app/api.service';
 import { ConfirmPasswordValidator } from 'src/app/confirm-password.validator';
@@ -11,18 +12,27 @@ import { ConfirmPasswordValidator } from 'src/app/confirm-password.validator';
 })
 export class HeaderComponent implements OnInit {
 changePassowrdForm!:FormGroup
-  constructor(private formbuilder:FormBuilder,private apiService:ApiService,private alertService:AlertService) { }
+  constructor(private formbuilder:FormBuilder,private apiService:ApiService,private alertService:AlertService,private router:Router) { }
 
   ngOnInit(): void {
 
   }
   changePasswordForm=this.formbuilder.group({
-currentPwd:[''],
-newPwd:[''],
-cPwd:['']
+currentPwd:['',[Validators.required,
+Validators.minLength(8),
+Validators.maxLength(25),
+Validators.pattern(
+  "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9 \\!\"#\\$%&'\\(\\)\\*\\+,\\-\\.\\/\\:;\\<\\=\\>\\?@\\[\\\\\\]\\^_`\\{\\|\\}~]+$")]],
+newPwd:['',[Validators.required,
+  Validators.minLength(8),
+  Validators.maxLength(25),
+  Validators.pattern(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9 \\!\"#\\$%&'\\(\\)\\*\\+,\\-\\.\\/\\:;\\<\\=\\>\\?@\\[\\\\\\]\\^_`\\{\\|\\}~]+$"
+  )]],
+cPwd:['',Validators.required]
   },
   {
-    validator: ConfirmPasswordValidator("currentPwd", "cPwd")
+    validator: ConfirmPasswordValidator("newPwd", "cPwd")
   }as AbstractControlOptions );
   register(){
     if(this.changePasswordForm.valid){
@@ -38,13 +48,25 @@ cPwd:['']
                 console.log(result);
   
                 this.alertService.showSuccess("password changed successfully","Success")
-              
+                localStorage.clear();
+                this.router.navigate(["/login"]); 
        
     },
     error: (err: any) => {
       
       console.log(err);
-      this.alertService.showError("Failed to change password","Error")
+      console.log(err.error.errors[0].code);
+      switch (err.error.errors[0].code) {
+        case "104":
+          this.alertService.showError(
+            "Current password and new password are same",
+            "Error"
+          );
+          break;
+        case "105":
+          this.alertService.showError("Current password is wrong", "Error");
+          break;
+      }
     }  
     });
     }}}
