948a99975038bc30506841968d5f0d25e7ed8211
inv-lakshmi-mohan <120701295+inv-lakshmi-mohan@users.noreply.github.com>
2023-03-06 11:31:23 +0530

    Delete Server directory

--- Server/src/main/java/com/airline/reservation/security/util/InvalidTokenException.java
+++
@@ -1,9 +0,0 @@
-package com.airline.reservation.security.util;
-public class InvalidTokenException extends IllegalArgumentException {
-    public InvalidTokenException(String s) {
-        super(s);
-    }
-    public InvalidTokenException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
